ad1e43a73069ccaf7e9748470e6bf0fad5e2238f
BurhanAmedie <55028983+BurhanAmedie@users.noreply.github.com>
2021-04-01 23:11:23 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB0avDlRlo2eBwaI0QMbrCIvFBnBDN8wpI",
-    authDomain: "whatsupclone-44b1b.firebaseapp.com",
-    projectId: "whatsupclone-44b1b",
-    storageBucket: "whatsupclone-44b1b.appspot.com",
-    messagingSenderId: "31038165833",
-    appId: "1:31038165833:web:b83ef6ac233e0537364098"
+    apiKey: ,
+    authDomain: ,
+    projectId: ,
+    storageBucket: ,
+    messagingSenderId: ,
+    appId: 
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
